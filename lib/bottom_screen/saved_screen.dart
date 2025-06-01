aa4b0a8c45f2d22af45efc3528be914dc94a9347
TenZinKS <tenzinkuenzel2016@gmail.com>
2025-06-01 21:59:18 +0545

    Added Saved Screen bottom navigation bar

---
+++ lib/bottom_screen/saved_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SavedScreen extends StatelessWidget {
+  const SavedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Saved Trips"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+    );
+  } 
+}
