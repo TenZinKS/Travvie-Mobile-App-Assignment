c92013630990ccf78db8fd6bc229c948a19f37a3
TenZinKS <tenzinkuenzel2016@gmail.com>
2025-06-01 21:58:02 +0545

    Added Explore Screen bottom navigation bar

---
+++ lib/bottom_screen/explore_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ExploreScreen extends StatelessWidget {
+  const ExploreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Explore"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+    );
+  }
+}
