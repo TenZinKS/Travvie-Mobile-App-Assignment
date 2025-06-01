09456d7ff8822a6bb52839fa9dcf4db70fe8ae1e
TenZinKS <tenzinkuenzel2016@gmail.com>
2025-06-01 21:50:41 +0545

    Updated app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:travvie/view/home_page_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travvie/cubit/splash_screen_cubit.dart';
+import 'package:travvie/theme/theme.dart';
+import 'package:travvie/view/dashboard.dart';
 import 'package:travvie/view/login_view.dart';
 import 'package:travvie/view/signup_view.dart';
 import 'package:travvie/view/splash_screen_view.dart';
+
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-      debugShowCheckedModeBanner: false,
-      title: 'Travvie',
-      
-      initialRoute: '/',
-      routes: {
-        '/': (context) =>  SplashScreenView(),
-        '/login': (context) => const LoginView(),
-        '/homepage': (context) => const HomePageView(),
-        '/signup': (context) => const SignupView(),
-      },
+    return BlocProvider(
+      create: (_) => SplashScreenCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Travvie',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const SplashScreenView(),
+          '/login': (context) => const LoginView(),
+          '/homepage': (context) => const Dashboard(),
+          '/signup': (context) => const SignupView(),
+        },
+        theme: getApplicationTheme(),
+      ),
     );
   }
 }
