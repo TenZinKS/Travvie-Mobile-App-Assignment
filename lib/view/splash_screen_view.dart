6d06bcecc13f37599b1b64808973ea270506bad0
TenZinKS <tenzinkuenzel2016@gmail.com>
2025-06-01 21:48:47 +0545

    Used Cubit for SplashScreen

--- lib/view/splash_screen_view.dart
+++ lib/view/splash_screen_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'login_view.dart'; // Navigate to login after splash
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travvie/cubit/splash_screen_cubit.dart';
 
 class SplashScreenView extends StatefulWidget {
   const SplashScreenView({super.key});
@@ -13,47 +13,52 @@ class _SplashScreenViewState extends State<SplashScreenView> {
   @override
   void initState() {
     super.initState();
-    // Redirect to Login screen after 2 seconds
-    Timer(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const LoginView()),
-      );
+
+    // Delay for 2 seconds, then trigger cubit
+    Future.delayed(const Duration(seconds: 2), () {
+      context.read<SplashScreenCubit>().completeSplash();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255), // main color
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // App Logo
-            Image.asset(
-              'assets/logo/logo.png',
-              width: 500,
-              height: 500,
-            ),
-
-            // App Name
-            const Text(
-              'Travvie',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                letterSpacing: 2,
+    return BlocListener<SplashScreenCubit, bool>(
+      listener: (context, state) {
+        if (state) {
+          Navigator.pushReplacementNamed(context, '/login');
+        }
+      },
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // App Logo
+              Image.asset(
+                'assets/logo/logo.png',
+                width: 500,
+                height: 500,
               ),
-            ),
-            const SizedBox(height: 30),
 
-            // Loading Indicator
-            const CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
-            ),
-          ],
+              // App Name
+              const Text(
+                'Travvie',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  letterSpacing: 2,
+                ),
+              ),
+              const SizedBox(height: 30),
+
+              // Loading Indicator
+              const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+              ),
+            ],
+          ),
         ),
       ),
     );
