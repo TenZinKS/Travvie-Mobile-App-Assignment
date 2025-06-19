777661b0c11bc8ced5e2fa4246d9c57ee4b4b655
TenZinKS <tenzinkuenzel2016@gmail.com>
2025-06-19 09:28:00 +0545

    Register Adapter and Initialize Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:travvie/app/app.dart';
+import 'features/auth/data/model/user_model.dart';
 
-void main(){
-    runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserModelAdapter());
+
+  runApp(const App());
 }
